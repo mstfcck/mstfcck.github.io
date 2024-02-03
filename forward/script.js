d060b5fc5dc90dca72c7a422886ac1d369594998
Mustafa Çiçek <mstfcck@outlook.com>
2024-02-03 15:31:56 +0100

    Forward

---
+++ forward/script.js
@@ -0,0 +1,40 @@
+const urlMappings = new Map([
+    ["mstfcckofficial-youtube", "https://www.youtube.com/@mstfcckofficial"],
+    ["mstfcckofficial-instagram", "https://www.instagram.com/mstfcckofficial"],
+    ["mstfcckofficial-tiktok", "https://www.tiktok.com/@mstfcckofficial"],
+    ["mstfcckofficial-facebook", "https://www.facebook.com/mstfcckofficial"],
+    ["mstfcckofficial-twitter", "https://twitter.com/mstfcckofficial"],
+    ["mstfcckofficial-threads", "https://www.threads.net/@mstfcckofficial"],
+]);
+
+// Check for the "to" parameter in the query string
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if (urlParams.has('to')) {
+    // Get the value of the "to" parameter
+    const toValue = urlParams.get('to');
+
+    // Check if the "to" value is in the cache
+    if (urlMappings.has(toValue)) {
+        const redirectTo = urlMappings.get(toValue);
+
+        // Display the countdown before redirecting
+        let countdownValue = 3;
+        const countdownElement = document.getElementById('countdown');
+
+        const countdownInterval = setInterval(() => {
+            countdownElement.textContent = countdownValue;
+            countdownValue--;
+
+            if (countdownValue < 0) {
+                // Redirect to the matched URL when countdown reaches 0
+                clearInterval(countdownInterval);
+                window.location.replace(redirectTo)
+            }
+        }, 1000);
+    } else {
+        // Display an alert if no match found
+        alert("No matching URL found for the given 'to' value.");
+    }
+}
