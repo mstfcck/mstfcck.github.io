4356254ec15c3f982f279ad99658898bd844dfe3
Mustafa Çiçek <mstfcck@outlook.com>
2024-02-06 04:00:51 +0100

    Links are updated.

--- links/script.js
+++ links/script.js
@@ -5,6 +5,20 @@ const urlMappings = new Map([
     ["mstfcckofficial-facebook", "https://www.facebook.com/mstfcckofficial"],
     ["mstfcckofficial-twitter", "https://twitter.com/mstfcckofficial"],
     ["mstfcckofficial-threads", "https://www.threads.net/@mstfcckofficial"],
+
+    ["jobs-in-belgium-actiris-most-demand-and-shortage-occupations", "https://www.actiris.brussels/lijst-beroepen-die-rekruteren/jobs"],
+    
+    ["jobs-in-belgium-vdab-most-demand-and-shortage-occupations", "https://www.vdab.be/trends/knelpuntberoepen"],
+    ["jobs-in-belgium-vdab-most-demand-and-shortage-occupations-list-2024-pdf", "https://www.vdab.be/sites/default/files/media/files/Knelpuntberoepen2024.pdf"],
+    ["jobs-in-belgium-vdab-nl-vind-een-job", "https://www.vdab.be/vindeenjob/vacatures"],
+    
+    ["jobs-in-belgium-forem-most-demand-and-shortage-occupations", "https://emploi.wallonie.be/home/travailleurs-etrangers/permis-de-travail/metiers-en-penurie-pour-travailleurs-etrangers.html"],
+    ["jobs-in-belgium-forem-most-demand-and-shortage-occupations-list-2024", "https://emploi.wallonie.be/home/travailleurs-etrangers/permis-de-travail/liste-des-metiers-en-penurie.html"],
+    ["jobs-in-belgium-forem-fr-rechercher-un-metier", "https://www.leforem.be/infos-metiers/metiers"],
+    
+    ["jobs-in-belgium-adg-most-demand-and-shortage-occupations", "https://adg.be/desktopdefault.aspx/tabid-5401/9347_read-50919"],
+    ["jobs-in-belgium-adg-most-demand-and-shortage-occupations-list-2024", "https://adg.be/PortalData/46/Resources/dokumente/artikel-dokumente/Mangelberufe_fuer_2023-2024a.pdf"],
+    ["jobs-in-belgium-adg-de-arbeitsuche", "https://adg.be/desktopdefault.aspx/tabid-6166"],
 ]);
 
 const queryString = window.location.search;
